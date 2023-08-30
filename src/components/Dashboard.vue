299eed5b2e6def3fe502720821b914c609b2a2e3
LuisBianchine <luishdb@hotmail.com>
2023-08-30 17:25:35 -0300

    Começando a implementar barra de pesquisa e paginação

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,27 +1,35 @@
 <template>
   <message :msg="msg" v-show="msg" />
-  <Form 
+  <Form
     :form="exibirformNew"
     @closemodal="exibirformNew = false"
     @get-produtos="GetProdutos"
   />
-  <Form 
+  <Form
     :form="exibirFormEdit"
     @closemodal="exibirFormEdit = false"
     @get-produtos="GetProdutos"
     :Buscar="FormProduto"
     :Editar="EditForm"
-  />  
-  <div id="botao-novo">
+  />
+
+  <div id="botao-novo-area">
     <div>
-      <v-btn
-        id="botao"
-        icon="mdi-plus"
-        size="large"
-        @click="exibirformNew = true"
-      ></v-btn>
+      <div id="search-area">
+        <Search id="search" />
+      </div>
+      <div>
+        <v-btn
+          id="botao-novo"
+          icon="mdi-plus"
+          size="large"
+          @click="exibirformNew = true"
+        >
+        </v-btn>
+      </div>
     </div>
   </div>
+
   <div id="tabela-produtos">
     <div>
       <div id="cabecalho-produtos">
@@ -33,7 +41,7 @@
       </div>
     </div>
     <div id="linhas-produtos">
-      <div class="linha" v-for="product in produtos" :key="product.id">
+      <div class="linha" v-for="product in produtos" :key="product.id" >
         <div class="order-number">{{ product.id }}</div>
         <div>{{ product.codigo_interno }}</div>
         <div>{{ product.nome }}</div>
@@ -41,31 +49,42 @@
         <div>{{ product.familia }}</div>
         <v-btn
           id="botao-editar"
-          icon="mdi-triangle"
+          icon="mdi-pencil"
           size="small"
-          @click="exibirFormEdit = true; BuscarProdutos(product.id); EditForm=true"
+          @click="
+            exibirFormEdit = true;
+            BuscarProdutos(product.id);
+            EditForm = true;
+          "
         ></v-btn>
         <v-btn
           id="botao-excluir"
-          icon="mdi-minus"
+          icon="mdi-delete"
           size="small"
           @click="DeleteProdutos(product.id)"
         ></v-btn>
-      </div>
     </div>
-  </div>
+</div>
+<div>
+  <v-pagination v-model="Pagination.page" :length="PageCount"></v-pagination>
+</div>    
+</div>
+  
 </template>
 
 <script>
 import axios from "axios";
 import message from "./Message.vue";
 import Form from "./Form.vue";
+import Search from "./Search.vue";
+import { computed } from 'vue';
 
 export default {
   name: "dashboard",
   components: {
     message,
     Form,
+    Search,
   },
   data() {
     return {
@@ -82,7 +101,12 @@ export default {
         unidade: null,
         familia: null,
       },
-      EditForm: false
+      EditForm: false,
+      Pagination: {
+        page: 1,
+        itemsperPage: 10,
+        PageCount: null
+      },
     };
   },
   methods: {
@@ -90,11 +114,22 @@ export default {
       const config = {
         method: "GET",
         url: "http://localhost:9000/produtos",
+        data: {
+            id: this.produtos.id,
+            cod_empresa: this.produtos.cod_empresa,
+            nome: this.produtos.nome,
+            unidade: this.produtos.unidade,
+            codigo_interno: this.produtos.codigo_interno,
+            familia: this.produtos.familia
+        }
       };
 
       axios(config)
         .then((res) => {
           this.produtos = res.data;
+          
+          this.PageCount = (res.data.length / this.Pagination.itemsperPage)
+              
         })
         .catch((error) => {
           console.error(error);
@@ -131,10 +166,9 @@ export default {
       };
       axios(config)
         .then((res) => {
-            if(res.status == 200){
-               this.FormProduto = res.data[0]
-
-            }
+          if (res.status == 200) {
+            this.FormProduto = res.data[0];
+          }
         })
         .catch((error) => {
           alert("Erro ao tentar recuperar os registros");
@@ -167,6 +201,10 @@ export default {
   width: 20%;
 }
 
+#cabecalho-produtos {
+  color: dimgray;
+}
+
 .linha {
   width: 100%;
   padding: 12px;
@@ -184,23 +222,38 @@ export default {
   border-bottom: 3px solid #08c4bc;
 }
 
-#botao-novo {
+#botao-novo-area {
   justify-content: flex-end;
   display: flex;
   margin-right: 50px;
 }
 
-#botao {
+#botao-novo-area div {
+  display: flex;
+}
+
+#botao-novo {
   background-color: #08c4bc;
   color: white;
   font-weight: bold;
 }
 
 #botao-excluir {
-  background-color: red;
-  color: white;
+  background-color: white;
+  color: red;
   font-weight: bold;
   display: flex;
   margin-left: 10px;
+}
+
+#botao-editar {
+  color: dimgray;
+}
+
+#search {
+  padding: 0;
+  height: 55px;
+  margin-right: 10px;
+  min-width: 300px;
 }
 </style>
